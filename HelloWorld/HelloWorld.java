acecfe1005771669e9174a206dd79aec5ca932ce
Ismi Nuraeni Januarita <isminuraaenij@student.telkomuniversity.ac.id>
2020-09-17 20:37:00 +0700

    HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Ismi Nuraeni Januarita");
+		System.out.println("NIM : 6706201019");
+		System.out.println("Kelas : D3-IF-44-03");
+	}
+}
